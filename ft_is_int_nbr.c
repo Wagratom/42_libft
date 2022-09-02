534b42e0a9c245ef983aee55db290959c99353ca
wwallas- <wwallas-@student.42sp.org.br>
2022-09-02 16:58:44 +0200

    pu

--- ft_is_int_nbr.c
+++ ft_is_int_nbr.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_is_not_nbr.c                                    :+:      :+:    :+:   */
+/*   ft_is_int_nbr.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wwallas- <wwallas-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 15:56:14 by wwallas-          #+#    #+#             */
-/*   Updated: 2022/09/02 16:26:31 by wwallas-         ###   ########.fr       */
+/*   Updated: 2022/09/02 16:58:30 by wwallas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,12 @@
 int	ft_is_int_nbr(char *nbr)
 {
 	char	*conversion;
+	int		failure;
 
+	failure = 0;
 	conversion = ft_itoa(ft_atoi(nbr));
 	if (ft_strncmp(conversion, nbr, ft_strlen(nbr) + 1))
-		return (1);
-	return (0);
+		failure = 1;
+	free(conversion);
+	return (failure);
 }
