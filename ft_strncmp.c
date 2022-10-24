541c03d61c3941da45fa1920ed0a194f31cd9731
wallas <wagratom111@gmail.com>
2022-10-24 18:16:38 -0300

    refactor ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: wwallas- <wwallas-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 16:36:16 by wwallas-          #+#    #+#             */
-/*   Updated: 2022/06/02 17:34:59 by wwallas-         ###   ########.fr       */
+/*   Updated: 2022/10/24 18:14:49 by wwallas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,12 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	count;
-
-	count = 0;
 	if (!s1 || !s2 || !n)
 		return (0);
-	while ((s1[count] || s2[count]) && count < n - 1)
+	while ((*s1 || *s2) &&  --n)
 	{
-		if (s1[count] != s2[count])
-			return ((unsigned char) s1[count] - (unsigned char)s2[count]);
-		count++;
+		if (*s1++ != *s2++)
+			return ((unsigned char)*(s1 -1) - (unsigned char)*(s2- 1));
 	}
-	return ((unsigned char) s1[count] - (unsigned char)s2[count]);
+	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
