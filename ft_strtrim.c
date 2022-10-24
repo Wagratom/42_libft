fcae1e991af56ebd384eebc51f217ba46457ad4b
wallas <wagratom111@gmail.com>
2022-10-24 18:40:22 -0300

    refactor strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,56 +6,44 @@
 /*   By: wwallas- <wwallas-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 16:36:55 by wwallas-          #+#    #+#             */
-/*   Updated: 2022/06/04 03:41:44 by wwallas-         ###   ########.fr       */
+/*   Updated: 2022/10/24 18:40:02 by wwallas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	end_str(char const *copy, char const *set, int size)
+static t_bool	has_set(char const *s1, char const *set)
 {
-	int		count;
-	int		flag;
-	size_t	i;
+	while (*set)
+	{
+		if (ft_strchr(set, *s1))
+			return (TRUE);
+		set++;
+	}
+	return (FALSE);
+}
+
+static int	end_str(char const *s1, char const *set, int size)
+{
+	char const	*end_s1;
+	int			count;
 
 	count = 0;
-	copy += size;
-	while (size--)
+	end_s1 = s1 + size;
+	while (size-- && end_s1--)
 	{
-		copy--;
-		i = 0;
-		flag = 0;
-		while (set[i])
-		{
-			if (*copy == set[i])
-			{
-				count++;
-				flag = 1;
-			}
-			i++;
-		}
-		if (flag == 0)
-			return (count);
+		if (!has_set(end_s1, set))
+			break ;
+		count ++;
 	}
 	return (count);
 }
 
-static char	const	*start_check(char const *s1, char const *set)
+static char	const	*start_check_set(char const *s1, char const *set)
 {
-	size_t	i;
-	int		flag;
-
 	while (*s1)
 	{
-		i = 0;
-		flag = 0;
-		while (set[i])
-		{
-			if (*s1 == set[i])
-				flag = 1;
-			i++;
-		}
-		if (flag == 0)
+		if (!has_set(s1, set))
 			break ;
 		s1++;
 	}
@@ -67,7 +55,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 	char	*result;
 	int		del;
 
-	s1 = start_check(s1, set);
+	s1 = start_check_set(s1, set);
 	del = end_str(s1, set, ft_strlen(s1));
 	result = ft_substr(s1, 0, (ft_strlen(s1) - del));
 	if (!result)
