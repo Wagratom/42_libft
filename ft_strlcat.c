e42214504e75d197e3368168f839b4a2e0649cf1
wallas <wagratom111@gmail.com>
2022-10-24 19:00:36 -0300

    pausa

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: wwallas- <wwallas-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 16:35:37 by wwallas-          #+#    #+#             */
-/*   Updated: 2022/05/31 16:35:41 by wwallas-         ###   ########.fr       */
+/*   Updated: 2022/10/24 18:58:46 by wwallas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t	s_dest;
+	size_t	len_dst;
 	size_t	s_src;
 	size_t	i;
 
-	s_dest = ft_strlen(dst);
-	if (size <= s_dest)
+	len_dst = ft_strlen(dst);
+	if (size <= len_dst)
 		return (ft_strlen(src) + size);
-	size -= s_dest + 1;
+	size -= len_dst + 1;
 	i = -1;
 	while (++i < size && src[i])
-		dst[s_dest + i] = src[i];
-	dst[s_dest + i] = '\0';
+		dst[len_dst + i] = src[i];
+	dst[len_dst + i] = '\0';
 	s_src = ft_strlen(src);
-	return (s_src + s_dest);
+	return (s_src + len_dst);
 }
